bcdb4495e0926410c11d65a0d3eacbd1ae51ac44
sonTung Nguyen <nguyensontungtdn1901@gmail.com>
2024-07-14 22:35:17 +0700

    chore: adding comments

--- core.js
+++ core.js
@@ -11,10 +11,13 @@ export default function html([first, ...strings], ...values) {
 }
 
 export function createStore(reducer) {
+  // As an object may have many states
+  // Ex: cars, price, users, ... Choose a state to be displayed
   let state = reducer();
 
   const roots = new Map();
 
+  // Render it out in `VIEW`
   function render() {
     for (const [root, component] of roots) {
       root.innerHTML = component();
@@ -27,6 +30,8 @@ export function createStore(reducer) {
       render();
     },
 
+    // Select which `state` will be displayed
+    // in the following "selector"
     connect(selector = (state) => state) {
       return (component) =>
         (props, ...args) =>
