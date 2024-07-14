ef578d2b1dcfb70f086250d5e9650ffa0fa9706b
sonTung Nguyen <nguyensontungtdn1901@gmail.com>
2024-07-14 18:32:42 +0700

    add: add function to handle STORE with `attach`, `connect`, and `dispatch`

--- core.js
+++ core.js
@@ -9,3 +9,32 @@ export default function html([first, ...strings], ...values) {
     .filter((x) => x && x !== true)
     .join("");
 }
+
+export function createStore(reducer) {
+  let state = reducer();
+
+  const roots = new Map();
+
+  function render() {
+    for (const [root, component] of roots) {
+      root.innerHTML = component();
+    }
+  }
+
+  return {
+    attach(component, root) {
+      roots.set(root, component);
+      render();
+    },
+
+    connect(selector = (state) => state) {
+      return (component) =>
+        (props, ...args) =>
+          component(Object.assign({}, props, selector(state), ...args));
+    },
+    dispatch(action, ...args) {
+      state = reducer(state, action, args);
+      render();
+    },
+  };
+}
